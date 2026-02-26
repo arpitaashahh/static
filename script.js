b61dd82d24b595f38992645b60cee5c794f60dc8
arpitaashahh <arpitaa1625@gmail.com>
2026-02-26 15:02:34 +0530

    new files

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    const msg = document.getElementById("message");
+    msg.innerText = "JavaScript is working 🎉";
+}
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
